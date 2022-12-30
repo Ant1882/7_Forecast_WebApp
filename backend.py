bae2c091407ea9dad417e16f34fd6ce6dfec72c8
Ant1882 <antmarshall1882@gmail.com>
2022-12-30 20:36:21 +0000

    switch to metric units

--- backend.py
+++ backend.py
@@ -3,7 +3,7 @@ import requests
 API_KEY = "35d680a6c62b8846fdd3fccc567a1b73"
 
 def get_data(place, forecast_days=None):
-    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&units=metric&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
     # 40 Values over 5 days, 8 values per day (3hr intervals)
