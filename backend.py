4c1b4924c42e8062c9a2eb388bedaa8ed709022f
Ant1882 <antmarshall1882@gmail.com>
2022-12-30 18:55:17 +0000

    Added basic request

---
+++ backend.py
@@ -0,0 +1,13 @@
+import requests
+
+API_KEY = "35d680a6c62b8846fdd3fccc567a1b73"
+
+def get_data(place, forecast_days=None, kind=None):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    return data
+
+
+if __name__ == "__main__":
+    print(get_data(place="Tokyo"))
